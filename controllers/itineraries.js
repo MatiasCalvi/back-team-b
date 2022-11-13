8c55b520ccaa0b32ae02bcb50edba983567f4c4f
MatiasCalvi <alejandropreuss30@gmail.com>
2022-11-13 05:13:59 -0300

    mt-b1-35

--- controllers/itineraries.js
+++ controllers/itineraries.js
@@ -58,7 +58,24 @@ const controller = {
                 message: error.message
             })
         }
-    }
+    },
+    destroy: async(req,res) => {
+        let { id } = req.params
+        try {
+            let itinerary = await Itinerary.findOneAndDelete({ _id: id })
+            if (itinerary) {
+                res.status(200).json({
+                    success: true,
+                    message: "itinerary has been deleted"
+                })
+            } 
+        } catch(error) {
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
+        }
+      }
   };
   module.exports = controller;
   
--- routes/api/itineraries.js
+++ routes/api/itineraries.js
@@ -1,9 +1,10 @@
 let router = require("express").Router();
 
-let { readOne,create,update } = require("../../controllers/itineraries");
+let { readOne,create,update,destroy } = require("../../controllers/itineraries");
 
 router.get("/", readOne);
 router.post("/", create);
 router.put("/:id", update);
+router.delete("/:id",destroy)
 
 module.exports = router;
