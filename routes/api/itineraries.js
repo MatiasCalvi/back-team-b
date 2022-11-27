26782a64dcaaa68f16d693249a7e07798f4e009a
MatiasCalvi <alejandropreuss30@gmail.com>
2022-11-27 03:42:05 -0300

    mt-01-53

--- routes/api/itineraries.js
+++ routes/api/itineraries.js
@@ -1,9 +1,12 @@
 let router = require("express").Router();
+const validator= require('../../middlewares/validator')
+const passport = require('../../config/passport')
+const schema=require('../../schemas/loginUser')
 
 let { create,update,destroy,read } = require("../../controllers/itineraries");
 
 router.get("/", read);
-router.post("/", create);
+router.post("/",passport.authenticate('jwt',{session:false}),validator(schema),create);
 router.put("/:id", update);
 router.delete("/:id",destroy)
 
