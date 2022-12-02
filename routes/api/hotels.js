4a6deb2daed3b0e0bd9b37f9324e82bcd7d9ead7
MatiasCalvi <114601077+MatiasCalvi@users.noreply.github.com>
2022-12-02 06:34:58 -0300

    Update hotels.js

--- routes/api/hotels.js
+++ routes/api/hotels.js
@@ -2,14 +2,13 @@ let router  = require('express').Router();
 let validator=require("../../middlewares/validator")
 let schema=require('../../schemas/newHotel')
 const passport = require('../../config/passport')
-/* let schema2= require ('../../schemas/deleteHotel') */
 
-let { create, read, update, destroy,  } = require("../../controllers/hotels");
+
+let { create, read, update, destroy } = require("../../controllers/hotels");
 
 router.route("/")     
 .post(validator(schema), passport.authenticate("jwt", { session: false }) ,create)  
 router.get("/",read);
-router.get("/ /?userId=id",readOne);
 router.patch("/:id", update)
 router.route("/:id")     
 .delete(destroy)  
