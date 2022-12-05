c6bba46dda5f82125cc1593c342018f57a5620d6
MatiasCalvi <alejandropreuss30@gmail.com>
2022-12-05 10:13:02 -0300

    fix

--- routes/api/hotels.js
+++ routes/api/hotels.js
@@ -4,12 +4,11 @@ let schema=require('../../schemas/newHotel')
 const passport = require('../../config/passport')
 /* let schema2= require ('../../schemas/deleteHotel') */
 
-let { create, read, update, destroy,  } = require("../../controllers/hotels");
+let { create, read, update, destroy  } = require("../../controllers/hotels");
 
 router.route("/")     
 .post(validator(schema), passport.authenticate("jwt", { session: false }) ,create)  
 router.get("/",read);
-router.get("/ /?userId=id",readOne);
 router.patch("/:id", update)
 router.route("/:id")     
 .delete(destroy)  
